09d0bce288741bacb333db468d0981133532f52b
Moealsir <mohamedwdalsir@gmail.com>
2024-07-11 22:46:38 +0300

    3-glam_rock.sql

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,9 @@
+-- glam rock
+SELECT band_name, 
+  CASE 
+	WHEN split IS NULL THEN 2022 - formed    
+	ELSE split - formed
+END AS lifespan
+FROM metal_bands 
+WHERE style = 'Glam rock'
+ORDER BY lifespan DESC LIMIT 100;
