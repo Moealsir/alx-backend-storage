c6b77a1c0b93d0a4ebec72889950249a1e6f94ea
Moealsir <mohamedwdalsir@gmail.com>
2024-07-12 00:01:17 +0300

    5-valid_email.sql

--- 0x00-MySQL_Advanced/4-main.sql
+++ 0x00-MySQL_Advanced/4-main.sql
@@ -1,14 +1,14 @@
--- Initial
-DROP TABLE IF EXISTS users;
+-- Show users and update (or not) email
+SELECT * FROM users;
 
-CREATE TABLE IF NOT EXISTS users (
-    id int not null AUTO_INCREMENT,
-    email varchar(255) not null,
-    name varchar(255),
-    valid_email boolean not null default 0,
-    PRIMARY KEY (id)
-);
+UPDATE users SET valid_email = 1 WHERE email = "bob@dylan.com";
+UPDATE users SET email = "sylvie+new@dylan.com" WHERE email = "sylvie@dylan.com";
+UPDATE users SET name = "Jannis" WHERE email = "jeanne@dylan.com";
 
-INSERT INTO users (email, name) VALUES ("bob@dylan.com", "Bob");
-INSERT INTO users (email, name, valid_email) VALUES ("sylvie@dylan.com", "Sylvie", 1);
-INSERT INTO users (email, name, valid_email) VALUES ("jeanne@dylan.com", "Jeanne", 1);
+SELECT "--";
+SELECT * FROM users;
+
+UPDATE users SET email = "bob@dylan.com" WHERE email = "bob@dylan.com";
+
+SELECT "--";
+SELECT * FROM users;
