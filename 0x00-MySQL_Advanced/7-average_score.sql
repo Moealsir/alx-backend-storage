dbe6adc014d6dc358cab764dea1c1861b956e5fe
Moealsir <mohamedwdalsir@gmail.com>
2024-07-12 00:34:56 +0300

    7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,14 +1,12 @@
 -- average score
-DELIMITER $$
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
-
-CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser(
+    IN user_id INT)
 BEGIN
-  DECLARE avg_score FLOAT;
-  SET avg_score = (
-    SELECT AVG(score) 
-    FROM corrections 
-    WHERE corrections.user_id = user_id);
-    UPDATE users SET average_score - avg_score WHERE id = user_id;
-END $$
+    DECLARE avg_score FLOAT;
+    SET avg_score = (SELECT AVG(score) FROM corrections AS C WHERE C.user_id=user_id);
+    UPDATE users SET average_score = avg_score WHERE id=user_id;
+END
+$$
 DELIMITER ;
