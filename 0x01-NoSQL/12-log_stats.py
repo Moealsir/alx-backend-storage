9da66aab41ee3f794cd37971645e0b7fd1c25f72
Moealsir <mohamedwdalsir@gmail.com>
2024-07-16 16:59:31 +0300

    12-log_stats.py

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -10,19 +10,19 @@ def print_nginx_request_logs(nginx_collection):
     print('Methods:')
     methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
     for method in methods:
-        req_count = nginx_collection.count_documents({'method': method})
+        req_count = len(list(nginx_collection.find({'method': method})))
         print('\tmethod {}: {}'.format(method, req_count))
-        status_checks_count = nginx_collection.count_documents(
-            {'method': method, 'path': '/status'}
-        )
-        print('{} status check'.format(status_checks_count))
+    status_checks_count = len(list(
+        nginx_collection.find({'method': 'GET', 'path': '/status'})
+    ))
+    print('{} status check'.format(status_checks_count))
 
 
 def run():
-    """Entry point."""
+    """Provides some stats about Nginx logs stored in MongoDB."""
     client = MongoClient('mongodb://127.0.0.1:27017')
     print_nginx_request_logs(client.logs.nginx)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     run()
