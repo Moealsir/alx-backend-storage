2015fa6c6be72264c2b07132afa12fe31043328d
Moealsir <mohamedwdalsir@gmail.com>
2024-07-16 16:38:40 +0300

    9-insert_school.py

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -2,7 +2,6 @@
 """Module for 9-insert_school.py"""
 
 
-
 def insert_school(mongo_collection, **kwargs):
     """Inserts a new document in a collection."""
     result = mongo_collection.insert_one(kwargs)
